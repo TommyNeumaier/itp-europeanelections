f2c691478e7342eb71079bea50ebb3cbcab6b2e9
julianjaklitsch <j.jaklitsch@aon.at>
2024-04-29 11:59:49 +0200

    Einfach Alles gemacht

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EuropaWahlComponent} from "./europa-wahl/europa-wahl.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EuropaWahlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
